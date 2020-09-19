507524dbc2b14816d2354eb772d923d4a6b8a753
ysl <yvesysl@protonmail.com>
2020-09-19 13:45:36 +0300

    Project bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Napkin from "./components/napkin";
+
+function App() {
+  return (
+    <div className={'container-fluid'}>
+        <Napkin />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/napkin.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Napkin = () => {
+    return (
+        <div className={'row'}>
+
+        </div>
+    );
+};
+
+export default Napkin;
